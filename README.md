1d345ecee23157725fe1ec17111096816731fcf8
Александр <den180777@mail.ru>
2023-04-09 17:00:10 +0300

    added push script

--- README.md
+++ README.md
@@ -4,4 +4,35 @@
 # Usage:
 ```
 python transform.py usernames.txt passwords.txt outcredentials.json
+python push.py <-f(ront),-b(ack)> credentials.json Username Password
 ```
+
+### Example what's going on:
+    ** usernames.txt **
+    ```
+    admin
+    root
+    ```
+    ** passwords.txt **
+    ```
+    admin
+    qwerty12345
+    toor
+    root
+    ```
+    ** Enterning command: **
+    ```
+    python transform.py usernames.txt passwords.txt outcredentials.json
+    ```
+
+    ** outcredentials.json **
+    ```
+    {
+        "usernames":
+        ["admin", root],
+        "passwords":
+        ["admin", "qwerty12345", "toor", "root"]
+    }
+    ```
+
+Big lists you can find on: https://github.com/danielmiessler/SecLists
---
+++ push.py
@@ -0,0 +1,38 @@
+import os
+import json
+import sys
+
+def push_front(cred, username, password):
+    print("Pushing front...\n")
+    cred_json = json.loads(open(cred, "r+").read())
+    if not username in cred_json["usernames"]:
+        cred_json["usernames"].insert(0, username)
+    if not password in cred_json["passwords"]:
+        cred_json["passwords"].insert(0, password)
+    cred_file = open(cred, "w+")
+    cred_file.write(json.dumps(cred_json))
+    cred_file.close()
+    print("Successfuly pushed front.")
+def push_back(cred, username, password):
+    print("Pushing back...\n")
+    cred_json = json.loads(open(cred, "r+").read())
+    if not username in cred_json["usernames"]:
+        cred_json["usernames"].append(username)
+    if not password in cred_json["passwords"]:
+        cred_json["passwords"].append(password)
+    cred_file = open(cred, "w+")
+    cred_file.write(json.dumps(cred_json))
+    cred_file.close()
+    print("Successfuly pushed back.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 4:
+        if str(sys.argv[1]).startswith("-f"):
+            push_front(sys.argv[2], sys.argv[3], sys.argv[4])
+        elif str(sys.argv[1]).startswith("-b"):
+            push_front(sys.argv[2], sys.argv[3], sys.argv[4])
+        else:
+            print(f"Unknown option {sys.argv[1]}.")
+    else:
+        print("Not enough arguments.")
